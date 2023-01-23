260ab5883d37e020c45757204d6f3f4747bdbb31
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-23 19:31:51 -0300

    igualdade e identidade

--- extrator.py
+++ extrator.py
@@ -1,5 +1,6 @@
 import re
 
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -15,11 +16,9 @@ class ExtratorURL:
         if not self.url:
             raise ValueError("A URL está vazia")
 
-        url = 'https://www.bytebank.com.br/cambio'
         padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
-        padrao_url.match(url)
-
-        if not padrao_url.match(url):
+        match = padrao_url.match(self.url)
+        if not match:
             raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
@@ -32,7 +31,7 @@ class ExtratorURL:
         url_parametros = self.url[indice_interrogacao + 1:]
         return url_parametros
 
-    def get_valor_parametro(self, parametro_busca):
+    def get_valor_parametro(self, parametro_busca):  # Método para buscar o valor de um parâmetro
         indice_parametro = self.get_url_parametros().find(parametro_busca)
         indice_valor = indice_parametro + len(parametro_busca) + 1
         indice_e_comercial = self.get_url_parametros().find('&', indice_valor)
@@ -42,7 +41,19 @@ class ExtratorURL:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
 
+    def __len__(self):  # Sobrescrevendo o método de tamanho
+        return len(self.url)
 
-extrator_url = ExtratorURL("bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar")
+    def __str__(self):  # Sobrescrevendo o método de conversão para string
+        return self.url + "\n" + "Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, other):  # Sobrescrevendo o método de comparação
+        return self.url == other.url
+
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
+extrator_url = ExtratorURL(url)
+print("O tamanho da URL é: ", len(extrator_url.url))
+print(extrator_url)
+
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
