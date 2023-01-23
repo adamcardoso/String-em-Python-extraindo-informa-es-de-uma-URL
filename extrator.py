4058ac12f453c6197e34c118f10892c9b8e2cd9d
adamcardoso <adamjuniorcardoso@gmail.com>
2023-01-23 19:16:54 -0300

    Expressões regulares

--- extrator.py
+++ extrator.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self.url = self.sanitiza_url(url)
@@ -12,6 +14,13 @@ class ExtratorURL:
     def valida_url(self):
         if not self.url:
             raise ValueError("A URL está vazia")
+
+        url = 'https://www.bytebank.com.br/cambio'
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        padrao_url.match(url)
+
+        if not padrao_url.match(url):
+            raise ValueError("A URL não é válida.")
 
     def get_url_base(self):
         indice_interrogacao = self.url.find('?')
